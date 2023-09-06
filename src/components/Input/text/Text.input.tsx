53c5047984f6ac2ff7a58387549cc92490781878
Ilies Benosman <47740197+iliesben@users.noreply.github.com>
2023-09-06 15:54:28 +0200

    Refactor: mantine (#19)
    * feat: add withCSSVariables to MantineProvider
    
    * feat: base matine core file
    
    * feat: use mantine base file
    
    * chore: remove not used file
    
    ---------
    
    Co-authored-by: Ilies <benosman.ilies@hetic.net>

--- src/components/Input/text/Text.input.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import { TextInput as MantineTextInput, TextInputProps } from '@mantine/core';
-
-export const TextInput = (props: TextInputProps) => (
-  <MantineTextInput {...props} />
-);

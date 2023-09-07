d052c173d5bd23ac0b366560ac39de875c5566f1
Ange <ange.goua01@gmail.com>
2023-09-07 19:57:10 +0200

    fix(Home): clean style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,39 @@
-import { MainBlock } from './main.block';
+import { Button } from '@/components/Button';
+import { HorizontalNav } from '@/components/Nav/Horizontal.nav';
+import { Box, Container, Flex, Text, Title } from '@/components/Mantine';
+import { BsArrowRight } from 'react-icons/bs';
 
-export default function Landing() {
-  return <MainBlock />;
+export default function Home() {
+  return (
+    <>
+      <HorizontalNav />
+      <Box mt={'10%'} mb={'xl'}>
+        <Container size="md" pb={'lg'}>
+          <Title order={1} size={'4rem'} ta="center">
+            Simplify your virtual machine configuration effortlessly
+          </Title>
+        </Container>
+        <Container size="sm" pb={'lg'}>
+          <Text size="md" ta="center">
+            Focus on what matters the most, save time and configure your virtual
+            machine in few clicks. You choose, we provide all you need
+          </Text>
+        </Container>
+        <Flex justify="center" align="center" direction="column" pb={'lg'}>
+          <Box pb={'md'}>
+            <Button redirectTo={'/register'}>Get started</Button>
+          </Box>
+          <Box>
+            <Button
+              variant="arrow"
+              rightIcon={<BsArrowRight />}
+              redirectTo="/community"
+            >
+              See community configuration
+            </Button>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
 }

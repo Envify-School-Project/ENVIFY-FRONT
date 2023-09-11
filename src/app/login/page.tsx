e4867c7cca125917f7098bc31dc22cf7138e1439
Ilies <benosman.ilies@hetic.net>
2023-09-11 13:18:57 +0200

    feat: use auth container

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,31 +1,10 @@
-import { Features } from '@/components/Form/Features';
 import { LoginForm } from '@/components/Form/Login.form';
-import { ButtonIcon } from '@/components/Button';
-import { Box, Flex, Container } from '@/components/Mantine';
-import { GiHexagonalNut } from 'react-icons/gi';
+import { AuthContainer } from '@/components/Container/Auth.container';
 
 export default function Login() {
   return (
-    <>
-      <Flex align="center" className="full-vh">
-        <Box
-          className="h-100"
-          sx={{
-            backgroundColor: 'var(--mantine-color-dark-5)',
-            borderRight: '1px solid var(--mantine-color-dark-4)',
-          }}
-        >
-          <Box p="md">
-            <ButtonIcon href={'/'}>
-              <GiHexagonalNut />
-            </ButtonIcon>
-          </Box>
-          <LoginForm />
-        </Box>
-        <Container size="sm">
-          <Features />
-        </Container>
-      </Flex>
-    </>
+    <AuthContainer>
+      <LoginForm />
+    </AuthContainer>
   );
 }

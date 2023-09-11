78d039a4de6858fdd16cd2b42a3551878f3afb91
Ilies <benosman.ilies@hetic.net>
2023-09-11 12:50:45 +0200

    refactor: baseInput style

--- src/components/Input/baseInput.style.ts
+++ src/components/Input/baseInput.style.ts
@@ -1,28 +1,42 @@
-import { MantineTheme } from '@mantine/core';
+import { MantineTheme, rem } from '@mantine/core';
 
 export const baseInputStyle = {
   styles: (theme: MantineTheme) => ({
     root: {
-      input: {
-        border: `1px solid ${theme.colors.dark[2]}`,
-        backgroundColor: `${theme.colors.dark[1]}`,
-        borderRadius: '5px',
-        color: `${theme.colors.dark[3]}`,
-      },
       label: {
-        color: theme.colors.dark[3],
-        fontSize: '14px',
-        marginBottom: '6px',
-      },
-      description: {
-        marginBottom: '4px',
+        color: theme.colors.dark[1],
+        fontSize: rem(14),
+        marginBottom: theme.spacing.xxs,
       },
     },
+    description: {
+      marginBottom: theme.spacing.xxs,
+    },
     input: {
+      height: rem(40),
       border: `1px solid ${theme.colors.dark[2]}`,
-      backgroundColor: `${theme.colors.dark[1]}`,
-      borderRadius: '5px',
-      color: `${theme.colors.dark[3]}`,
+      backgroundColor: `${theme.colors.dark[4]}`,
+      borderRadius: rem(5),
+      color: `${theme.white}`,
+      '&:focus, &:focus-within': {
+        borderColor: theme.white,
+      },
+      '&:-webkit-autofill, &:-webkit-autofill:hover, &:-webkit-autofill:focus':
+        {
+          '-webkit-text-fill-color': `${theme.white}`,
+          '-webkit-box-shadow': `0 0 0px 1000px ${theme.colors.dark[4]} inset`,
+        },
+    },
+    innerInput: {
+      backgroundColor: 'transparent',
+      height: rem(40),
+      color: `${theme.white}`,
+      '&:-webkit-autofill, &:-webkit-autofill:hover, &:-webkit-autofill:focus':
+        {
+          '-webkit-text-fill-color': `${theme.white}`,
+          '-webkit-box-shadow': `0 0 0px 1000px ${theme.colors.dark[4]} inset`,
+          border: 0,
+        },
     },
   }),
 };

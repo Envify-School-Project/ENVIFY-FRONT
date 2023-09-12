a33a296845ef886dd54766bb7d616f058cb1fdaa
Ange <ange.goua01@gmail.com>
2023-09-12 21:18:04 +0200

    feat(Dashboard): init layout and did sidebar

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import { Sidebar } from '@/components/Nav/Sidebar';
+import { AppShell, AppShellMain } from '@mantine/core';
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <AppShell navbar={{ width: 250, breakpoint: 'sm' }} pt="xl">
+        <Sidebar />
+        <AppShellMain ml={80}>{children}</AppShellMain>
+      </AppShell>
+      <></>
+    </>
+  );
+}
---
+++ src/components/Nav/Sidebar/index.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import classes from './Sidebar.module.css';
+import { AppShellNavbar, Box, Text } from '@mantine/core';
+
+export const Sidebar = () => {
+  return (
+    <AppShellNavbar className={classes.navbar} bg="dark.6" withBorder>
+      <Text component="p" c="white" mb={56} px="xs">
+        Dashboard
+      </Text>
+      <Text component="p" c="dark.3" px="xs">
+        Configurations
+      </Text>
+      {/* TODO: Create all links and make loops */}
+      <Box component={Link} p="xs" href="/" className={classes.link}>
+        My configurations
+      </Box>
+    </AppShellNavbar>
+  );
+};

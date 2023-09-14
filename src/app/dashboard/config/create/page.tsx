1527749e7e0f22b805d3a899009f7fffba225f6d
angegoua <ange.goua01@gmail.com>
2023-09-14 16:25:09 +0200

    feat(Stepper): clean it

--- src/app/dashboard/config/create/page.tsx
+++ src/app/dashboard/config/create/page.tsx
@@ -1,31 +1,14 @@
-import { Stepper, StepperCompleted, StepperStep, Title } from '@mantine/core';
+import { Title } from '@mantine/core';
+import { Stepper } from '@/components/Nav/Stepper';
 
 export default function CreateConfig() {
   return (
     <>
-      <Title order={1}>Create new config</Title>
-      <Stepper active={0}>
-        <StepperStep
-          label="Choose a config name"
-          description="Create an account"
-        >
-          step 1: {/* Input for config name */}
-        </StepperStep>
-        <StepperStep
-          label="Select your modules"
-          description="Container, packages, db..."
-        >
-          step 2:
-          {/* modules list  */}
-        </StepperStep>
-        <StepperStep
-          label="Generate your scripts"
-          description="Copy, paste in your VM and voila"
-        >
-          Step 3:
-          {/* loading scripts */}
-        </StepperStep>
-        <StepperCompleted>{/* script view */}</StepperCompleted>
+      <Title order={1} mb="xl">
+        Create new config
+      </Title>
+      <Stepper>
+        <p>hey</p>
       </Stepper>
     </>
   );
--- src/components/Nav/Stepper/index.tsx
+++ src/components/Nav/Stepper/index.tsx
@@ -1,43 +1,44 @@
 'use client';
 import React, { useState } from 'react';
-import { Stepper as StepperContainer, Group } from '@mantine/core';
-import { Button } from '@/components/Button';
+import { Stepper as StepperContainer, ScrollArea, Flex } from '@mantine/core';
+import { Button } from '@mantine/core';
 import classes from './Stepper.module.css';
 
 type StepperProps = {
+  children: React.ReactNode;
   type?: 'create' | 'update';
 };
 
-export const Stepper = ({ type = 'create' }: StepperProps) => {
-  const [active, setActive] = useState(1);
-  const [highestStepVisited, setHighestStepVisited] = useState(active);
+const stepData = {
+  create: [
+    {
+      label: 'Choose a config name',
+      description: 'To save and identify it',
+    },
+    {
+      label: 'Select your modules',
+      description: 'Container, packages, db... ',
+    },
+    {
+      label: 'Generate your scripts',
+      description: 'Copy, paste in your VM and tada',
+    },
+  ],
+  update: [
+    {
+      label: 'Remove or add modules',
+      description: 'Container, packages, db...',
+    },
+    {
+      label: 'Regenerate your scripts',
+      description: 'Copy, paste in your VM and tada',
+    },
+  ],
+};
 
-  const stepData = {
-    create: [
-      {
-        label: 'Choose a config name',
-        description: 'To save and identify it',
-      },
-      {
-        label: 'Select your modules',
-        description: 'Container, packages, db... ',
-      },
-      {
-        label: 'Generate your scripts',
-        description: 'Copy, paste in your VM and tada',
-      },
-    ],
-    update: [
-      {
-        label: 'Remove or add modules',
-        description: 'Container, packages, db...',
-      },
-      {
-        label: 'Regenerate your scripts',
-        description: 'Copy, paste in your VM and tada',
-      },
-    ],
-  };
+export const Stepper = ({ children, type = 'create' }: StepperProps) => {
+  const [active, setActive] = useState(0);
+  const [highestStepVisited, setHighestStepVisited] = useState(active);
 
   const handleStepChange = (nextStep: number) => {
     const isOutOfBounds = nextStep > 3 || nextStep < 0;
@@ -55,39 +56,53 @@ export const Stepper = ({ type = 'create' }: StepperProps) => {
 
   return (
     <>
-      <StepperContainer
-        color="violet"
-        radius="sm"
-        size="md"
-        active={active}
-        onStepClick={setActive}
-        m="xs"
-        classNames={{
-          separator: classes.stepperSeparator,
-          stepIcon: classes.stepperBtn,
-          step: classes.stepperStep,
-        }}
-      >
-        {stepData[type].map((step, index) => (
-          <StepperContainer.Step
-            label={step.label}
-            description={step.description}
-            allowStepSelect={shouldAllowSelectStep(index)}
-            c="white"
-          />
-        ))}
-        <StepperContainer.Completed>
-          Completed, click back button to get to previous step
-        </StepperContainer.Completed>
-      </StepperContainer>
-      <Group p="center" mt="xl">
-        <Button variant="outline" onClick={() => handleStepChange(active - 1)}>
-          Back
-        </Button>
-        <Button variant="filled" onClick={() => handleStepChange(active + 1)}>
-          Next step
-        </Button>
-      </Group>
+      <Flex direction="column" gap="lg">
+        <StepperContainer
+          color="violet"
+          radius="sm"
+          size="md"
+          active={active}
+          onStepClick={setActive}
+          classNames={{
+            separator: classes.stepperSeparator,
+            stepIcon: classes.stepperBtn,
+            step: classes.stepperStep,
+          }}
+        >
+          {stepData[type].map((step, index) => (
+            <StepperContainer.Step
+              key={index}
+              label={step.label}
+              description={step.description}
+              allowStepSelect={shouldAllowSelectStep(index)}
+              c="white"
+            />
+          ))}
+          <StepperContainer.Completed>
+            Completed, click back button to get to previous step
+          </StepperContainer.Completed>
+        </StepperContainer>
+
+        <ScrollArea
+          classNames={{
+            root: classes.scrollArea,
+          }}
+        >
+          {children}
+        </ScrollArea>
+
+        <Flex justify="flex-end" gap="md" p="center" py="xl">
+          <Button
+            variant="outline"
+            onClick={() => handleStepChange(active - 1)}
+          >
+            Back
+          </Button>
+          <Button variant="filled" onClick={() => handleStepChange(active + 1)}>
+            Next step
+          </Button>
+        </Flex>
+      </Flex>
     </>
   );
 };

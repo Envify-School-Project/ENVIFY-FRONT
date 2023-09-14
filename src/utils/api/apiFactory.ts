ee11d132f54a3120ff4d38545f2c0fdc1aab5934
Ilies <benosman.ilies@hetic.net>
2023-09-14 14:32:54 +0200

    feat: create apiFactory using fetch

---
+++ src/utils/api/apiFactory.ts
@@ -0,0 +1,54 @@
+const responseStatusHandler = (status: number) => {
+  if (status === 401) {
+    // gestion de l'erreur d'authentification
+    return Promise.reject(new Error('Unauthorized'));
+  }
+};
+
+const responseErrorHandler = (error: unknown) => {
+  // Gérez les erreurs  ici
+  return Promise.reject(error);
+};
+
+const apiFactory = (baseUrl: string) => ({
+  get: async <TOutput>(
+    path: string,
+    options: RequestInit = {}
+  ): Promise<TOutput> => {
+    try {
+      const response = await fetch(`${baseUrl}${path}`, options);
+
+      responseStatusHandler(response.status);
+
+      return response.json() as Promise<TOutput>;
+    } catch (error) {
+      return responseErrorHandler(error);
+    }
+  },
+
+  post: async <TInput, TOutput>(
+    path: string,
+    data: TInput,
+    options: RequestInit = {}
+  ): Promise<TOutput> => {
+    try {
+      const response = await fetch(`${baseUrl}${path}`, {
+        ...options,
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          ...options.headers,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      responseStatusHandler(response.status);
+
+      return response.json() as Promise<TOutput>;
+    } catch (error) {
+      return responseErrorHandler(error);
+    }
+  },
+});
+
+export const apiClient = apiFactory(process.env.NEXT_PUBLIC_API_URL);

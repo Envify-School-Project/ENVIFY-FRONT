a9ad012e55b373f9ddfcf219ce47ca724479f19c
Ilies Benosman <47740197+iliesben@users.noreply.github.com>
2023-10-02 21:50:04 +0200

    Feat: stepper form (#37)
    * feat: create Stepper provider
    
    * feat: create Stepper and Step components
    
    * feat: update Stepper style
    
    * feat: create stepperButton
    
    * feat: remove not used component

---
+++ src/components/Stepper/StepperButton.tsx
@@ -0,0 +1,42 @@
+import { Button } from '@/components/Button';
+import { Group } from '@mantine/core';
+import { useStepperContext } from './Stepper.provider';
+import { useConfigFormContext } from '@/app/dashboard/config/create/configForm.context';
+
+type StepperButtonsProps = {
+  stepsValidation?: (string | string[])[];
+};
+
+export const StepperButtons = ({
+  stepsValidation,
+}: StepperButtonsProps): JSX.Element => {
+  const form = useConfigFormContext();
+
+  const { activeStep, prevStep, nextStep } = useStepperContext();
+
+  const isValidate = () => {
+    if (!stepsValidation) return false;
+    const currentStepFields = stepsValidation[activeStep];
+
+    if (currentStepFields instanceof Array) {
+      const errors = currentStepFields.map((field) => form.isValid(field));
+
+      return errors.every((value) => !value);
+    }
+
+    return !form.isValid(currentStepFields);
+  };
+
+  return (
+    <Group justify="end" mt="xl">
+      {activeStep > 0 ? (
+        <Button onClick={prevStep} variant="outline" type="submit">
+          Précédent
+        </Button>
+      ) : null}
+      <Button onClick={nextStep} disabled={isValidate()} type="submit">
+        Confirmer
+      </Button>
+    </Group>
+  );
+};

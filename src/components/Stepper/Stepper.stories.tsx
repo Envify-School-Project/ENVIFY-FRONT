82cb6e094596dcae8f7a6f0c1782c982689d87d8
Ilies Benosman <47740197+iliesben@users.noreply.github.com>
2023-10-03 12:57:52 +0200

    Feat: create config form (#38)
    * feat: create config form context
    
    * feat: create ConfigName Block component
    
    * feat: create SelectPackage Block component
    
    * feat: update Select style
    
    * feat: update CardCheckbox style
    
    * feat: stepper.stories
    
    * fix: config style and type
    
    * feat: update Mantine

---
+++ src/components/Stepper/Stepper.stories.tsx
@@ -0,0 +1,75 @@
+import type { StoryObj, Meta } from '@storybook/react';
+import { Step, Stepper } from './Stepper';
+import { Container } from '@mantine/core';
+import { StepperFormProvider, StepperProvider } from './Stepper.provider';
+import { StepperButtons } from './StepperButton';
+import { ConfigNameBlock } from '@/app/dashboard/config/create/block/ConfigName.block';
+import { SelectPackage } from '@/app/dashboard/config/create/block/SelectPackage.block';
+import { UseFormInput } from '@mantine/form/lib/types';
+import {
+  ConfigContext,
+  ConfigFormProvider,
+  useConfigForm,
+} from '@/app/dashboard/config/create/configForm.context';
+
+const meta: Meta<typeof StepperFormProvider> = {
+  title: 'components/Form/StepperForm',
+  component: StepperFormProvider,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      toc: true, // 👈 Enables the table of contents
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Container>
+        <Story />
+      </Container>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof StepperFormProvider>;
+
+const formInput: UseFormInput<ConfigContext> = {
+  initialValues: {
+    configName: '',
+    packages: [],
+  },
+  validate: {
+    configName: (value) =>
+      value && value?.length > 2 ? null : 'Invalid package name',
+    packages: (value) =>
+      value && value?.length > 0 ? null : 'Please select packages',
+  },
+};
+
+export const Default: Story = {
+  render: () => (
+    <StepperFormProvider
+      formInput={formInput}
+      FormProvider={ConfigFormProvider}
+      useForm={useConfigForm}
+    >
+      <StepperProvider>
+        <Stepper>
+          <Step
+            label="Choose a config name"
+            description="To save and identify it"
+          >
+            <ConfigNameBlock />
+          </Step>
+          <Step
+            label="Select your modules"
+            description="Container, packages, db... "
+          >
+            <SelectPackage />
+          </Step>
+        </Stepper>
+        <StepperButtons stepsValidation={['configName', 'packages']} />
+      </StepperProvider>
+    </StepperFormProvider>
+  ),
+};

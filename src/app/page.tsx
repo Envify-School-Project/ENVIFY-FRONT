50c2599f7c9ceff2ab10a9f9a342489785333efd
RayMiller7 <raymond@doohit.fr>
2023-10-06 01:03:12 +0200

    feat(landing-page): adding a new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,6 +36,56 @@ export default function Home() {
           </Box>
         </Flex>
       </Box>
+      <Box mt={150} mb={150}>
+        <Container pb="xl">
+          <Title order={2} size={48} ta="center">
+            Deliver Infrastructure as Code
+          </Title>
+        </Container>
+        <Container pb="xl">
+          <Flex justify="space-between" align="center" >
+            <Box mr="xl" >
+              <img src="../static/server-hosting-data-analyst_18660-996-fotor-bg-remover-202310060156.png" alt="Infrastructure as Code illustration" style={{ width: '600px', height: '400px', borderRadius: '8px' }} />
+            </Box>
+
+            <Flex direction="row">
+              <Box pr="xl" mb="xl" >
+                <Title order={3} size={24} mb="md">
+                  Bloc de texte 1
+                </Title>
+                <Text size="md" pb="xl">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </Text>
+
+                <Title order={3} size={24} mb="md">
+                  Bloc de texte 2
+                </Title>
+                <Text size="md" pb="xl">
+                  Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </Text>
+              </Box>
+
+              <Box mb="xl">
+                <Title order={3} size={24} mb="md">
+                  Bloc de texte 3
+                </Title>
+                <Text size="md" pb="xl">
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                </Text>
+
+                <Title order={3} size={24} mb="md">
+                  Bloc de texte 4
+                </Title>
+                <Text size="md" pb="xl">
+                  Duis aute irure dolor in reprehenderit in voluptate velit.
+                </Text>
+              </Box>
+            </Flex>
+          </Flex>
+        </Container>
+      </Box>
+
+
     </>
   );
 }

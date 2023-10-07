5b125cf3559eca9615abf0167d09bc78b53b533a
iliesben <ilies.benosman@hetic.net>
2023-10-07 15:43:49 +0200

    feat: add DisplayValue

--- src/components/Card/CheckboxCard/CheckboxCard.stories.tsx
+++ src/components/Card/CheckboxCard/CheckboxCard.stories.tsx
@@ -1,7 +1,7 @@
 import type { StoryObj, Meta } from '@storybook/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { CheckboxCard } from '.';
-import { Group } from '@mantine/core';
+import { Group, SimpleGrid } from '@mantine/core';
 
 const meta: Meta<typeof CheckboxCard> = {
   title: 'components/Cards/CheckboxCard',
@@ -20,74 +20,115 @@ const meta: Meta<typeof CheckboxCard> = {
     version: {
       description: `Different version for one package.`,
     },
+    onChange: {
+      control: {
+        disable: true,
+      },
+    },
     image: {
       description: `Image linked to the package.`,
     },
   },
   decorators: [
-    (Story) => (
-      <Group>
-        <Story />
-      </Group>
-    ),
+    (Story) => {
+      return (
+        <Group>
+          <SimpleGrid>
+            <Story />
+          </SimpleGrid>
+        </Group>
+      );
+    },
   ],
 };
 export default meta;
 type Story = StoryObj<typeof CheckboxCard>;
 
 export const Default: Story = {
-  render: () => (
-    <>
-      <CheckboxCard
-        title={'Nodejs'}
-        image={'https://nodejs.org/static/images/logo.svg'}
-        version={['10.0', '11.0', '12.0']}
-        onChange={function (
-          configName: string,
-          version: string,
-          checked: boolean
-        ): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
-    </>
-  ),
+  render: function Render() {
+    const { value, handleChange } = useCheckboxOnChange();
+
+    return (
+      <>
+        <CheckboxCard
+          title={'Nodejs'}
+          image={'https://nodejs.org/static/images/logo.svg'}
+          version={['10.0', '11.0', '12.0']}
+          onChange={handleChange}
+        />
+
+        <DisplayValue value={value} />
+      </>
+    );
+  },
 };
 
 export const WithoutImage: Story = {
-  render: () => (
-    <>
-      <CheckboxCard
-        title={'Nodejs'}
-        image={''}
-        version={['10.0', '11.0', '12.0']}
-        onChange={function (
-          configName: string,
-          version: string,
-          checked: boolean
-        ): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
-    </>
-  ),
+  render: function Render() {
+    const { value, handleChange } = useCheckboxOnChange();
+
+    return (
+      <>
+        <CheckboxCard
+          title={'Nodejs'}
+          image={''}
+          version={['10.0', '11.0', '12.0']}
+          onChange={handleChange}
+        />
+        <DisplayValue value={value} />
+      </>
+    );
+  },
 };
 
 export const WithoutVersion: Story = {
-  render: () => (
-    <>
-      <CheckboxCard
-        title={'Nodejs'}
-        image={'https://nodejs.org/static/images/logo.svg'}
-        version={[]}
-        onChange={function (
-          configName: string,
-          version: string,
-          checked: boolean
-        ): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
-    </>
-  ),
+  render: function Render() {
+    const { value, handleChange } = useCheckboxOnChange();
+
+    return (
+      <>
+        <CheckboxCard title={'Nodejs'} version={[]} onChange={handleChange} />
+        <DisplayValue value={value} />
+      </>
+    );
+  },
+};
+
+type CardCheckBoxOnChange = {
+  packageName: string;
+  packageVersion: string;
+  checked: boolean;
+};
+
+const DisplayValue = ({
+  value,
+}: {
+  value: CardCheckBoxOnChange | undefined;
+}) => {
+  return (
+    <pre style={{ marginTop: 10, color: 'black' }}>
+      {JSON.stringify({ value }, null, 2)}
+    </pre>
+  );
+};
+
+const useCheckboxOnChange = () => {
+  const [value, setValue] = useState<CardCheckBoxOnChange | undefined>();
+
+  const handleChange = (
+    packageName: string,
+    packageVersion: string,
+    checked: boolean
+  ) => {
+    setValue({
+      packageName: packageName,
+      packageVersion: packageVersion,
+      checked: checked,
+    });
+  };
+
+  return {
+    value,
+    handleChange,
+  };
 };

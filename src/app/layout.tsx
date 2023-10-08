24e428dae6d854263365cc5335aec31bd575c7e3
iliesben <ilies.benosman@hetic.net>
2023-10-08 18:43:53 +0200

    chore: move providers files to utils provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Theme } from '@/components/Theme';
 import './global.css';
 import '@mantine/core/styles.layer.css';
-import { ReactQueryClientProvider } from '@/components/providers/react-query.provider';
+import { ReactQueryClientProvider } from '@/utils/providers/react-query.provider';
 
 export default function RootLayout({
   children,

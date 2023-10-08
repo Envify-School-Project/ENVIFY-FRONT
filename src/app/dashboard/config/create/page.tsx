32bca068d4c29d239dd987a88b9fc78b2df42dce
iliesben <ilies.benosman@hetic.net>
2023-10-08 15:36:11 +0200

    feat: use ConfigCreateStepper

--- src/app/dashboard/config/create/page.tsx
+++ src/app/dashboard/config/create/page.tsx
@@ -1,29 +1,13 @@
-import { Grid, GridCol, Title } from '@mantine/core';
-import { Stepper } from '@/components/Nav/Stepper';
-import { apiClient } from '@/utils/api/apiFactory';
-import { Package } from '@/utils/types/config.type';
-import { CheckboxCard } from '@/components/Card/CheckboxCard';
-export default async function CreateConfig() {
-  const packages: Package[] = await apiClient.get('/packages.json');
+import { Title } from '@mantine/core';
+import { ConfigCreateStepper } from './ConfigCreate.stepper';
 
+export default async function CreateConfig() {
   return (
     <>
       <Title order={1} mb="xl">
         Create new config
       </Title>
-      <Stepper>
-        <Grid>
-          {packages?.map((pkg, index) => (
-            <GridCol span={4} key={index}>
-              <CheckboxCard
-                image={pkg.logo}
-                title={pkg.name}
-                version={pkg.version}
-              />
-            </GridCol>
-          ))}
-        </Grid>
-      </Stepper>
+      <ConfigCreateStepper />
     </>
   );
 }

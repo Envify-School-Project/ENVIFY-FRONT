f32f66edb3d2937a8ab1a3abc00b50128602e769
angegoua <ange.goua01@gmail.com>
2023-10-09 13:36:44 +0200

    fix(configs all): fset conditionnal display

--- src/app/dashboard/config/all/page.tsx
+++ src/app/dashboard/config/all/page.tsx
@@ -3,20 +3,24 @@ import type { Config } from '@/utils/types/config.type';
 import { Grid, GridCol, Title } from '@mantine/core';
 import { apiClient } from '@/utils/api/apiFactory';
 export default async function ConfigsList() {
-  const configs: Config[] = await apiClient.get('/configs.json');
+  const configs: Config[] = await apiClient.get('/configs');
 
   return (
     <>
       <Title order={1} mb="xl">
         Configurations
       </Title>
-      <Grid>
-        {configs.map((config) => (
-          <GridCol span={4} key={config.id}>
-            <ConfigCard config={config} />
-          </GridCol>
-        ))}
-      </Grid>
+      {configs?.length > 0 ? (
+        <Grid>
+          {configs?.map((config) => (
+            <GridCol span={4} key={config.id}>
+              <ConfigCard config={config} />
+            </GridCol>
+          ))}
+        </Grid>
+      ) : (
+        <p>You have no configurations yet.</p>
+      )}
     </>
   );
 }

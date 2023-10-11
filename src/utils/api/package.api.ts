1b557ce75659b98e7c1df036a1dae9a386fd40d9
iliesben <ilies.benosman@hetic.net>
2023-10-12 00:30:06 +0200

    fix: path and remove not used PackageVersions api

--- src/utils/api/package.api.ts
+++ src/utils/api/package.api.ts
@@ -1,8 +1,5 @@
-import { PackageDto, PackageVersionDto } from '../types/package.type';
+import { PackageDto } from '../types/package.type';
 import { apiClient } from './apiFactory';
 
 export const getPackages = async () =>
-  await apiClient.get<PackageDto[]>('/api/v1/packages');
-
-export const getPackageVersions = async (id: number) =>
-  await apiClient.get<PackageVersionDto[]>(`/api/v1/packages/${id}/versions`);
+  await apiClient.get<PackageDto[]>('/packages');

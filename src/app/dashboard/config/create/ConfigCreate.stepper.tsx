95360686bc6f239fb49853dce5d89f2623366359
iliesben <ilies.benosman@hetic.net>
2023-10-15 01:13:52 +0200

    feat: add postCofing api

--- src/app/dashboard/config/create/ConfigCreate.stepper.tsx
+++ src/app/dashboard/config/create/ConfigCreate.stepper.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Step, Stepper } from '@/components/Stepper/Stepper';
 import {
-  StepperFormProvider,
+  FormProvider,
   StepperProvider,
 } from '@/components/Stepper/Stepper.provider';
 import { ConfigNameBlock } from './block/ConfigName.block';
@@ -11,6 +11,11 @@ import { UseFormInput, hasLength, isNotEmpty } from '@mantine/form';
 import { ConfigFormProvider, useConfigForm } from './configForm.context';
 import { ConfigInput } from '@/utils/types/config.type';
 import { ConfigProperties } from './block/ConfigProperties.block';
+import { Box, StepperCompleted, Title } from '@mantine/core';
+import { ConfirmConfig } from './block/ConfirmConfig.block';
+import { postCofing } from '@/utils/api/config.api';
+import { useMutation } from 'react-query';
+import { BlockScript } from '@/components/BlockScript';
 
 export const configCreateFormInput: UseFormInput<ConfigInput> = {
   initialValues: {
@@ -25,54 +30,82 @@ export const configCreateFormInput: UseFormInput<ConfigInput> = {
   },
   validate: {
     name: hasLength(
-      { min: 3, max: 10 },
+      { min: 3, max: 50 },
       'the config name should be more than 2 characters long'
     ),
-    packages: isNotEmpty('Please Select a Package'),
     os: {
       versionId: hasLength({ min: 1 }, 'Please Select an Os'),
       name: hasLength({ min: 1 }, 'Please Select an Os'),
       versionNumber: hasLength({ min: 1 }, 'Please Select an Os version'),
     },
+    packages: {
+      name: isNotEmpty('Please Select a Package'),
+      packageProperties: {
+        properties: {
+          value: isNotEmpty('Need value'),
+        },
+      },
+    },
   },
 };
 
 export const ConfigCreateStepper = () => {
+  const { data, mutateAsync } = useMutation((configData: ConfigInput) =>
+    postCofing(configData)
+  );
+
   return (
-    <StepperFormProvider
+    <FormProvider
       formInput={configCreateFormInput}
       FormProvider={ConfigFormProvider}
       useForm={useConfigForm}
+      mutation={mutateAsync}
     >
-      <StepperProvider>
-        <Stepper>
-          <Step
-            label="Choose a config name"
-            description="To save and identify it"
-          >
-            <ConfigNameBlock />
-          </Step>
-          <Step
-            label="Select your modules"
-            description="Container, packages, db... "
-          >
-            <SelectPackage />
-          </Step>
-          <Step
-            label="Select you properties"
-            description="Choose your personal propeties"
-          >
-            <ConfigProperties />
-          </Step>
-        </Stepper>
-        <StepperButtons
-          stepsValidation={[
-            ['name', 'os.name', 'os.versionNumber'],
-            'packages',
-            'packagesProperties',
-          ]}
-        />
-      </StepperProvider>
-    </StepperFormProvider>
+      {data ? (
+        <BlockScript scripts={data.scripts} configFiles={data.configFiles} />
+      ) : (
+        <ConfigStepper />
+      )}
+    </FormProvider>
   );
 };
+
+const ConfigStepper = () => (
+  <Box>
+    <Title order={1} mb="xl">
+      Create your new config
+    </Title>
+    <StepperProvider>
+      <Stepper>
+        <Step
+          label="Choose a config name"
+          description="To save and identify it"
+        >
+          <ConfigNameBlock />
+        </Step>
+        <Step
+          label="Select your modules"
+          description="Container, packages, db... "
+        >
+          <SelectPackage />
+        </Step>
+        <Step
+          label="Select you properties"
+          description="Choose your personal propeties"
+        >
+          <ConfigProperties />
+        </Step>
+        <StepperCompleted>
+          <ConfirmConfig />
+        </StepperCompleted>
+      </Stepper>
+      <StepperButtons
+        stepsValidation={[
+          ['name', 'os.name', 'os.versionNumber'],
+          'packages.name',
+          'packages.packagesProperties',
+        ]}
+      />
+    </StepperProvider>
+  </Box>
+);

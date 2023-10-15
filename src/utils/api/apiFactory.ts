10035a2e3ae58be0d85d2fb8f11fdfa9bbf1c22d
angegoua <ange.goua01@gmail.com>
2023-10-15 07:05:14 +0200

    feat(apifactory): avoid to fetch session when it is login req

--- src/utils/api/apiFactory.ts
+++ src/utils/api/apiFactory.ts
@@ -50,7 +50,7 @@ const apiFactory = (baseUrl: string, type: ServerType = 'server') => ({
     options: RequestInit = {}
   ): Promise<TOutput> => {
     try {
-      const session = await getAuthSession(type);
+      const session = path === '/login' ? null : await getAuthSession(type);
       const response = await fetch(`${baseUrl}${path}`, {
         ...options,
         method: 'POST',
@@ -59,7 +59,7 @@ const apiFactory = (baseUrl: string, type: ServerType = 'server') => ({
           ...options.headers,
           'Content-Type': 'application/json',
           'ENVIFY-API-Key': `${process.env.NEXT_PUBLIC_ENVIFY_API_KEY}`,
-          Authorization: `Bearer ${session?.jwtToken}`,
+          Authorization: session ? `Bearer ${session.jwtToken}` : '',
         },
       });
 
